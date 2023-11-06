c186999e5328761a3d58743fa06648baed1b8ef4
Fabiola Z. Torres Maldonado <fabiola.torres28@upr.edu>
2023-11-05 22:25:07 -0400

    Removed duplicate files

--- Levels/Tutorial Level/Level Background Handler.gd
+++
@@ -1,8 +0,0 @@
-extends ParallaxBackground
-
-
-#controls the background movement.
-var Background__movementspeed = -0.001
-
-func _process(delta):
-	scroll_offset.x -= Background__movementspeed * delta
